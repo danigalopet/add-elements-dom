a2352e7ab00ababd03c20b3212a90cd5ee26fde9
Ignacio Alonso~ <danigalopet@gmail.com>
2024-06-12 08:37:34 +0200

    sprint3-challenge2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+ 
+    const lista = document.getElementById('lista');
+    const pulsar = document.getElementById('agregar');
+    const elemento = document.createElement("li");
+    
+  
+    
+    pulsar.addEventListener("click",function () {
+        const nuevo = prompt('Escriba un elemento:');                
+        nuevo ? lista.innerHTML = (nuevo) : lista.innerHTML= ("no has introducido ningun elemento")
+    })
+  
+    
+  
